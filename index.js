e75c9a5844d459092faed8923c7e47074e963195
Hannah Glazier <hrayglazier@aol.com>
2022-02-05 18:16:02 -0700

    commit tree 4284fec0ab0e4124f23eb81ab47f31e70ca54ad0

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 
 
-function addingEventListener() {
-    const input = document.getElementById('input');
 
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-    
-    input.addEventListener('click', clickAlert);
+function addingEventListener(){
+  const input = document.getElementById('input')
+  input.addEventListener('click', () => alert('I was clicked'))
 }
+
+
 
 
